b8114f291a592cf272d43eb8c4423bfc465905da
davidkenesaw <davidkennesaw@gmail.com>
2022-07-10 03:34:55 -0400

    new storage feature

---
+++ storage.lua
@@ -0,0 +1,199 @@
+potions = {"minecraft:potion{Potion:invisibility}","minecraft:potion{Potion:water_breathing}","minecraft:potion{Potion:harming}","minecraft:potion{Potion:slowness}","minecraft:potion{Potion:strength}","minecraft:potion{Potion:weakness}","minecraft:potion{Potion:night_vision}","minecraft:potion{Potion:healing}","minecraft:potion{Potion:poison}","minecraft:potion{Potion:fire_resistance}","minecraft:potion{Potion:swiftness}","minecraft:potion{Potion:regeneration}","minecraft:potion{Potion:water}","minecraft:potion"}
+blocks = {"minecraft:dirt","minecraft:oak_log","minecraft:chest","minecraft:cobbled_deepslate","minecraft:granite","minecraft:stick","minecraft:soul_sand","minecraft:sand","minecraft:crying_obsidian","minecraft:obsidian","minecraft:cobblestone","minecraft:stone","minecraft:quartz_stairs","minecraft:quartz_pillar","minecraft:chiseled_quartz_block","minecraft:quartz_block","minecraft:black_wool","minecraft:white_wool","minecraft:spruce_wood","minecraft:oak_wood","minecraft:jungle_planks","minecraft:birch_planks","minecraft:spruce_planks","minecraft:oak_planks"}
+tools = {"minecraft:arrow","minecraft:torch","minecraft:netherite_sword","minecraft:crossbow","minecraft:trident","minecraft:golden_sword","minecraft:diamond_sword","minecraft:stone_sword","minecraft:iron_sword","minecraft:netherite_hoe","minecraft:netherite_axe","minecraft:netherite_shovel","minecraft:totem_of_undying","minecraft:lead","minecraft:map","minecraft:shears","minecraft:filled_map","minecraft:clock","minecraft:fishing_rod","minecraft:compass","minecraft:golden_hoe","minecraft:diamond_hoe","minecraft:stone_hoe","minecraft:wooden_hoe","minecraft:golden_axe","minecraft:golden_pickaxe","minecraft:golden_shovel","minecraft:diamond_axe","minecraft:diamond_pickaxe","minecraft:diamond_shovel","minecraft:stone_axe","minecraft:stone_pickaxe","minecraft:stone_shovel","minecraft:wooden_axe","minecraft:wooden_pickaxe","minecraft:wooden_shovel","minecraft:bow","minecraft:flint_and_steel","minecraft:iron_axe","minecraft:iron_pickaxe","minecraft:iron_shovel"}
+foods = {"minecraft:baked_potato","minecraft:oak_sapling","minecraft:apple", "minecraft:bread", "minecraft:cooked_porkchop", "minecraft:golden_apple", "minecraft:cooked_cod", "minecraft:melon_slice", "minecraft:cooked_beef", "minecraft:cooked_chicken", "minecraft:carrot", "minecraft:melon_seeds","minecraft:pumpkin_seeds","minecraft:beetroot_seeds","minecraft:wheat","minecraft:wheat_seeds", "minecraft:potato", "minecraft:golden_carrot", "minecraft:cooked_rabbit", "minecraft:pumpkin_pie","minecraft:cooked_mutton","minecraft:beetroot",""}
+brewings = {"minecraft:glowstone_dust","minecraft:nether_wart","minecraft:glistering_melon_slice","minecraft:magma_cream","minecraft:blaze_powder","minecraft:fermented_spider_eye","minecraft:ghast_tear","minecraft:blaze_rod","minecraft:sugar"}
+ores = {"forbidden_arcanus:xpetrified_orb","forbidden_arcanus:arcane_crystal","minecraft:coal", "minecraft:diamond" , "minecraft:iron_ingot","minecraft:lapis_block","minecraft:gold_block","minecraft:iron_block","minecraft:diamond_block","minecraft:emerald_block","minecraft:coal_block","", "minecraft:gold_ingot", "minecraft:redstone", "minecraft:lapis_lazuli", "minecraft:gold_nugget", "minecraft:emerald", "minecraft:netherite_ingot", "minecraft:amethyst_shard", "minecraft:raw_gold", "minecraft:raw_iron"}
+drops = {"minecraft:glow_ink_sac","minecraft:rabbit_hide","minecraft:rotten_flesh","minecraft:bone","minecraft:egg","minecraft:slime_ball","minecraft:leather","minecraft:gunpowder","minecraft:string","minecraft:feather","minecraft:phantom_membrane","minecraft:rabbit_foot","minecraft:spider_eye"}
+armours = {"minecraft:netherite_leggings","minecraft:netherite_leggings","minecraft:netherite_chestplate","minecraft:netherite_helmet","minecraft:elytra","minecraft:shield","minecraft:diamond_horse_armor","minecraft:golden_horse_armor","minecraft:iron_horse_armor","minecraft:golden_boots","minecraft:golden_leggings","minecraft:golden_chestplate","minecraft:golden_helmet","minecraft:diamond_boots","minecraft:diamond_leggings","minecraft:diamond_chestplate","minecraft:diamond_helmet","minecraft:iron_boots","minecraft:iron_leggings","minecraft:iron_chestplate","minecraft:iron_helmet","minecraft:chainmail_boots","minecraft:chainmail_leggings","minecraft:chainmail_chestplate","minecraft:chainmail_helmet","minecraft:saddle"}
+
+function refuel()
+    for i = 1, 16 do
+        if findItem("minecraft:coal") == true then
+            turtle.refuel(1)
+            break
+        end
+    end
+    turtle.select(1)
+end 
+
+function forward(value)
+    
+    for i = 1, value do
+        if turtle.getFuelLevel() == 0 then
+            refuel()
+        end
+        turtle.dig()
+        turtle.forward()
+    end
+end
+
+function left(value)
+    for i = 1, value do 
+        turtle.turnLeft()
+    end
+end
+
+function right(value)
+    for i = 1, value do
+        turtle.turnRight()
+    end
+end
+
+function down(value)
+    
+    for i = 1, value do  
+        if turtle.getFuelLevel() == 0 then
+            refuel()
+        end 
+        turtle.digDown()
+        turtle.down()
+    end
+end
+
+function up(value)
+    
+    for i = 1, value do
+        if turtle.getFuelLevel() == 0 then
+            refuel()
+        end
+        turtle.digUp()
+        turtle.up()
+    end
+end
+
+function containsList(value, array)
+    for i = 1, #(array)do
+        if value == array[i] then
+            print(true)
+            return true
+        end
+    end
+    print(false)
+    return false
+end
+function store()
+    if turtle.drop() == false then
+        up(1)
+        if turtle.drop() == false then
+            up(1)
+            turtle.drop()
+            down(2)
+        else 
+            turtle.drop()
+            down(1)
+        end
+    else  
+        turtle.drop()
+    end
+
+end
+function scanInv()
+    position()
+    for i = 1, 15 do
+        data = turtle.getItemDetail(i)
+        turtle.select(i)
+        if data then
+            print("3")
+            if containsList(data.name, ores) == true then
+                ore(data.name)
+            elseif containsList(data.name, foods) == true then
+                food(data.name)
+            elseif containsList(data.name, brewings) == true then
+                brew(data.name)
+            elseif containsList(data.name, armours) == true then
+                armour(data.name)
+            elseif containsList(data.name, tools) == true then
+                tool(data.name)
+            elseif containsList(data.name, blocks) == true then
+                block(data.name)
+            elseif containsList(data.name, drops) == true then
+                drop(data.name)
+            elseif containsList(data.name, potions) == true then
+                potion(data.name)
+            end
+        end
+    end
+    positionBack()
+end
+
+function position()
+    forward(2)
+    left(1)
+    forward(5)
+end
+function positionBack()
+    left(2)
+    forward(5)
+    right(1)
+    forward(2)
+    left(2)
+end
+
+function ore(value)
+    left(2)
+    forward(3)
+    right(1)
+    store()
+    right(1)
+    forward(3)
+end
+function food(value)
+    left(2)
+    forward(3)
+    left(1)
+    store()
+    left(1)
+    forward(3)
+end
+function potion(value)
+    left(2)
+    forward(1)
+    left(1)
+    store()
+    left(1)
+    forward(1)
+end
+function tool(value)
+    left(2)
+    forward(1)
+    right(1)
+    store()
+    right(1)
+    forward(1)
+end
+function armour(value)
+    forward(1)
+    right(1)
+    store()
+    right(1)
+    forward(1)
+    left(2)
+end
+function drop(value)
+    forward(1)
+    left(1)
+    store()
+    left(1)
+    forward(1)
+    left(2)
+end
+function block(value)
+    forward(3)
+    left(1)
+    store()
+    left(1)
+    forward(3)
+    left(2)
+end
+function brew(value)
+    forward(3)
+    right(1)
+    store()
+    right(1)
+    forward(3)
+    left(2)
+end
+
+scanInv()
+--containsList("minecraft:diamond", ores)
