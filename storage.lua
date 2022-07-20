7a6b4ae204dd638c33d346f744fa58a63c736a89
davidkenesaw <davidkennesaw@gmail.com>
2022-07-20 17:30:31 -0400

    new storage function

--- storage.lua
+++ storage.lua
@@ -7,6 +7,8 @@ ores = {"forbidden_arcanus:xpetrified_orb","forbidden_arcanus:arcane_crystal","m
 drops = {"minecraft:glow_ink_sac","minecraft:rabbit_hide","minecraft:rotten_flesh","minecraft:bone","minecraft:egg","minecraft:slime_ball","minecraft:leather","minecraft:gunpowder","minecraft:string","minecraft:feather","minecraft:phantom_membrane","minecraft:rabbit_foot","minecraft:spider_eye"}
 armours = {"minecraft:netherite_leggings","minecraft:netherite_leggings","minecraft:netherite_chestplate","minecraft:netherite_helmet","minecraft:elytra","minecraft:shield","minecraft:diamond_horse_armor","minecraft:golden_horse_armor","minecraft:iron_horse_armor","minecraft:golden_boots","minecraft:golden_leggings","minecraft:golden_chestplate","minecraft:golden_helmet","minecraft:diamond_boots","minecraft:diamond_leggings","minecraft:diamond_chestplate","minecraft:diamond_helmet","minecraft:iron_boots","minecraft:iron_leggings","minecraft:iron_chestplate","minecraft:iron_helmet","minecraft:chainmail_boots","minecraft:chainmail_leggings","minecraft:chainmail_chestplate","minecraft:chainmail_helmet","minecraft:saddle"}
 
+all={"minecraft:potion{Potion:invisibility}","minecraft:potion{Potion:water_breathing}","minecraft:potion{Potion:harming}","minecraft:potion{Potion:slowness}","minecraft:potion{Potion:strength}","minecraft:potion{Potion:weakness}","minecraft:potion{Potion:night_vision}","minecraft:potion{Potion:healing}","minecraft:potion{Potion:poison}","minecraft:potion{Potion:fire_resistance}","minecraft:potion{Potion:swiftness}","minecraft:potion{Potion:regeneration}","minecraft:potion{Potion:water}","minecraft:potion","minecraft:glass","minecraft:glass_pane","minecraft:flint","minecraft:gravel","minecraft:oak_fence","minecraft:stone_bricks","minecraft:dirt","minecraft:oak_log","minecraft:chest","minecraft:cobbled_deepslate","minecraft:granite","minecraft:stick","minecraft:soul_sand","minecraft:sand","minecraft:crying_obsidian","minecraft:obsidian","minecraft:cobblestone","minecraft:stone","minecraft:quartz_stairs","minecraft:quartz_pillar","minecraft:chiseled_quartz_block","minecraft:quartz_block","minecraft:black_wool","minecraft:white_wool","minecraft:spruce_wood","minecraft:oak_wood","minecraft:jungle_planks","minecraft:birch_planks","minecraft:spruce_planks","minecraft:oak_planks","minecraft:arrow","minecraft:torch","minecraft:netherite_sword","minecraft:crossbow","minecraft:trident","minecraft:golden_sword","minecraft:diamond_sword","minecraft:stone_sword","minecraft:iron_sword","minecraft:netherite_hoe","minecraft:netherite_axe","minecraft:netherite_shovel","minecraft:totem_of_undying","minecraft:lead","minecraft:map","minecraft:shears","minecraft:filled_map","minecraft:clock","minecraft:fishing_rod","minecraft:compass","minecraft:golden_hoe","minecraft:diamond_hoe","minecraft:stone_hoe","minecraft:wooden_hoe","minecraft:golden_axe","minecraft:golden_pickaxe","minecraft:golden_shovel","minecraft:diamond_axe","minecraft:diamond_pickaxe","minecraft:diamond_shovel","minecraft:stone_axe","minecraft:stone_pickaxe","minecraft:stone_shovel","minecraft:wooden_axe","minecraft:wooden_pickaxe","minecraft:wooden_shovel","minecraft:bow","minecraft:flint_and_steel","minecraft:iron_axe","minecraft:iron_pickaxe","minecraft:iron_shovel","minecraft:baked_potato","minecraft:oak_sapling","minecraft:apple", "minecraft:bread", "minecraft:cooked_porkchop", "minecraft:golden_apple", "minecraft:cooked_cod", "minecraft:melon_slice", "minecraft:cooked_beef", "minecraft:cooked_chicken", "minecraft:carrot", "minecraft:melon_seeds","minecraft:pumpkin_seeds","minecraft:beetroot_seeds","minecraft:wheat","minecraft:wheat_seeds", "minecraft:potato", "minecraft:golden_carrot", "minecraft:cooked_rabbit", "minecraft:pumpkin_pie","minecraft:cooked_mutton","minecraft:beetroot","minecraft:glowstone_dust","minecraft:nether_wart","minecraft:glistering_melon_slice","minecraft:magma_cream","minecraft:blaze_powder","minecraft:fermented_spider_eye","minecraft:ghast_tear","minecraft:blaze_rod","minecraft:sugar","forbidden_arcanus:xpetrified_orb","forbidden_arcanus:arcane_crystal","minecraft:coal", "minecraft:diamond" , "minecraft:iron_ingot","minecraft:lapis_block","minecraft:gold_block","minecraft:iron_block","minecraft:diamond_block","minecraft:emerald_block","minecraft:coal_block","", "minecraft:gold_ingot", "minecraft:redstone", "minecraft:lapis_lazuli", "minecraft:gold_nugget", "minecraft:emerald", "minecraft:netherite_ingot", "minecraft:amethyst_shard", "minecraft:raw_gold", "minecraft:raw_iron","minecraft:glow_ink_sac","minecraft:rabbit_hide","minecraft:rotten_flesh","minecraft:bone","minecraft:egg","minecraft:slime_ball","minecraft:leather","minecraft:gunpowder","minecraft:string","minecraft:feather","minecraft:phantom_membrane","minecraft:rabbit_foot","minecraft:spider_eye","minecraft:netherite_leggings","minecraft:netherite_leggings","minecraft:netherite_chestplate","minecraft:netherite_helmet","minecraft:elytra","minecraft:shield","minecraft:diamond_horse_armor","minecraft:golden_horse_armor","minecraft:iron_horse_armor","minecraft:golden_boots","minecraft:golden_leggings","minecraft:golden_chestplate","minecraft:golden_helmet","minecraft:diamond_boots","minecraft:diamond_leggings","minecraft:diamond_chestplate","minecraft:diamond_helmet","minecraft:iron_boots","minecraft:iron_leggings","minecraft:iron_chestplate","minecraft:iron_helmet","minecraft:chainmail_boots","minecraft:chainmail_leggings","minecraft:chainmail_chestplate","minecraft:chainmail_helmet","minecraft:saddle",}
+
 function refuel()
     for i = 1, 16 do
         if findItem("minecraft:coal") == true then
@@ -118,6 +120,7 @@ function scanInv()
 end
 
 function position()
+    left(1)
     forward(2)
     left(1)
     forward(5)
@@ -127,7 +130,7 @@ function positionBack()
     forward(5)
     right(1)
     forward(2)
-    left(2)
+    left(1)
 end
 
 function ore(value)
@@ -195,5 +198,37 @@ function brew(value)
     left(2)
 end
 
-scanInv()
+function grabItems()
+    while(turtle.suck()==true)do
+        turtle.suck()
+    end
+end
+
+function spitItems()
+    for i = 1, 15 do
+        turtle.select(i)
+        data = turtle.getItemDetail(i)
+        if(data)then
+            if(containsList(data.name,all))then
+                turtle.select(i)
+                turtle.drop()
+            end
+        end
+    end
+end
+
+function scanChest()
+    
+    while true do
+        if(turtle.suck()==true)then 
+            grabItems()
+            scanInv()
+            spitItems()
+        end
+    end
+
+end
+
+scanChest()
+--scanInv()
 --containsList("minecraft:di
